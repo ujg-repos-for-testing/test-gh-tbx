4b402409858741504955e0861c9e70831788f050
ujg-repos-for-testing <60899052+ujg-repos-for-testing@users.noreply.github.com>
2020-04-30 14:16:39 -0400

    Add files via upload

---
+++ identity.m
@@ -0,0 +1,10 @@
+function X = identity(X)
+%IDENTITY Returns the value passed as an argument
+%   In mathematics, an identity function, also called an identity relation or identity map or
+%   identity transformation, is a function that always returns the same value that was used as its
+%   argument. In equations, the function is given by f(x) = x.
+%
+%   https://en.wikipedia.org/wiki/Identity_function
+
+    X;
+end
---
+++ sampleFunction.m
@@ -0,0 +1,8 @@
+function [Y] = sampleFunction(X)
+%sampleFunction Identity function.
+%   Y = sampleFunction(X) Returns X.
+%
+%   Example: Get the same Unicode character passed as the argument.
+%      star = sampleFunction('?');
+Y=X;
+end
